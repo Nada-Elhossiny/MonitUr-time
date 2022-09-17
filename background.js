c42e736b23d4c06d04f1cbd122762e7c9f58147a
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-09-17 16:24:49 -0400

    retrieve current url

--- background.js
+++ background.js
@@ -2,5 +2,16 @@ let colour = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ colour });
-  console.log('Default background colour set to %cgreen', 'color: ${colour}');
+  //console.log('Default background colour set to %cgreen', 'color: ${colour}');
 });
+
+chrome.tabs.query(
+  {
+    active: true,
+    currentWindow: true,
+  },
+  function (tabs) {
+    var tabURL = tabs[0].url;
+    console.log(tabURL);
+  }
+);
