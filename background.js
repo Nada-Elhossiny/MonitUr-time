e20e59644308604dd52aceb9d7387b2d1b39f4cc
Martin Zhao <martinzha28@gmail.com>
2022-09-17 21:38:32 -0400

    move db to popup

--- background.js
+++ background.js
@@ -1,21 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://monitur-time-default-rtdb.firebaseio.com/",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
-
-
-
 let colour = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
--- popup.js
+++ popup.js
@@ -1,20 +1,15 @@
-function getToday() {
-  return new Date().setHours(0, 0, 0, 0)
-}
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
-function getStartOfWeek() {
-  const now = new Date();
-  const day = now.getDay();
-  const diff = now.getDate() - day + (day === 0 ? -6 : 1); 
-  return new Date(now.setDate(diff)).setHours(0, 0, 0, 0);
-}
+const firebaseConfig = {
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://monitur-time-default-rtdb.firebaseio.com/",
+};
 
-function getStartOfPreviousWeek() {
-  const startOfWeek = new Date(getStartOfWeek())
-  return startOfWeek.setDate(startOfWeek.getDate() - 7)
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-function getEndOfPreviousWeek() {
-  const startOfWeek = new Date(getStartOfWeek())
-  return startOfWeek.setDate(startOfWeek.getDate() - 1)
-}
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
